7b4be9456f13d7911ddcd036b6244fead43ae961
Talkzy <keremalpdemir@icloud.com>
2024-10-26 10:59:49 +0200

    Add files via upload

---
+++ lightbulbproblem.py
@@ -0,0 +1,22 @@
+yournumber = int(input("What is your number? "))
+howmanydivisors = yournumber
+howmanydivisors = howmanydivisors + 1
+alldivisors = []
+divisors = []
+for i in range(howmanydivisors):
+    if i != 0:
+        alldivisors.append(i)
+for i in alldivisors:
+    if i == 0:
+        alldivisors.remove
+for i in alldivisors:
+    oddoreven = yournumber % i
+    if oddoreven == 0:
+        divisors.append(i)
+print("The divisors of your number are", divisors)
+x = len(divisors)
+y = x % 2
+if y == 0:
+    print("Light is off")
+if y != 0:
+    print("Light is on")
